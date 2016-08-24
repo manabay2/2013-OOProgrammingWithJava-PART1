5f3b61127782d1c28dc1286de4597930ad00965a
Eugen Milas <milas.eugen@gmail.com>
2016-08-25 01:37:49 +0300

    Add files via upload

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,13 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        int x=1;
+        while(true){
+            System.out.println(x);
+            x++;
+            if(x>100) break;
+            
+        }
+    }
+}
