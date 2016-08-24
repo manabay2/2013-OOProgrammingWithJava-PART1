aece5bea4f7f5059f6cca4bca7830b027c758f16
Eugen Milas <milas.eugen@gmail.com>
2016-08-25 01:39:45 +0300

    Add files via upload

---
+++ week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -0,0 +1,13 @@
+
+public class FromHundredToOne {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int x=100;
+        while(true){
+            System.out.println(x);
+            x--;
+            if(x<1)break;
+        }
+    }
+}
---
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -0,0 +1,14 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        // Write your code here
+        int x=2;
+        while (true){
+            System.out.println(x);
+            x+=2;
+            if(x>100)break;
+            
+        }
+    }
+}
